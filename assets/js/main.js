2e56b5286303bd067651641fc08661c7deeac5fc
vidongls <vidongls12345@gmail.com>
2021-08-16 17:30:53 +0700

    update js items

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,3 +139,112 @@ function animationScroll() {
   observer.observe(document.querySelector('.layout__image-boximg1'))
   observer.observe(document.querySelector('.layout__image-boximg2'))
 }
+//RenderSize
+
+//Render Products
+function renderProductsBestSeller() {
+  let newProducts = products.filter((product) => {
+    return product.category === 'best-sellers'
+  })
+
+  newProducts.forEach((val) => {
+    let template = `
+    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="products__list-item flex a-center j-center">
+                  <div class="products__list-item-imagebox">
+                    <img src="${val.urlImage}" alt="" />
+                    <div class="label ${val.status}">${val.status}</div>
+                    <div class="optionbox flex">
+                      <a href="" class="active"><i class="ph-eye"></i></a>
+                      <a href=""><i class="ph-heart-straight"></i></a>
+                      <a href=""><i class="ph-git-diff"></i></a>
+                    </div>
+                  </div>
+                  <div
+                    class="products__list-description flex a-center j-center"
+                  >
+                    <div class="text-box flex a-center j-center">
+                      <a class="title">${val.brand}</a>
+                      <span class="rating ${val.rate}">
+                        <div class="rate">
+                          <i class="fas fa-star"></i>
+                          <i class="fas fa-star"></i>
+                          <i class="fas fa-star"></i>
+                          <i class="fas fa-star"></i>
+                          <i class="fas fa-star"></i>
+                        </div>
+                        <div class="unrate">
+                          <i class="ph-star"></i>
+                          <i class="ph-star"></i>
+                          <i class="ph-star"></i>
+                          <i class="ph-star"></i>
+                          <i class="ph-star"></i>
+                        </div>
+                      </span>
+                      <div class="itemname">
+                        <a href="">${val.name}</a>
+                      </div>
+                      <div class="itemprice ${val.status}">
+                        <span>$${val.price}.00</span>
+                        <span class="itemprice-sale">
+                          <a href="">${val.sale}</a>
+                        </span>
+                      </div>
+                    </div>
+                    <div class="pattern">
+                      <ul class="flex j-center a-center">
+                        <li class="active">
+                          <a href=""
+                            ><img src="assets/img/img02.jpg" alt=""
+                          /></a>
+                        </li>
+                        <li>
+                          <a href=""
+                            ><img src="assets/img/img02.jpg" alt=""
+                          /></a>
+                        </li>
+                        <li>
+                          <a href=""
+                            ><img src="assets/img/img02.jpg" alt=""
+                          /></a>
+                        </li>
+                        <li>
+                          <a href=""
+                            ><img src="assets/img/img02.jpg" alt=""
+                          /></a>
+                        </li>
+                        <li>
+                          <a href=""
+                            ><img src="assets/img/img02.jpg" alt=""
+                          /></a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="products__list-size flex a-center j-center">
+                      <ul class="flex a-center j-center">               
+                      </ul>
+                    </div>
+                    <div class="products__list-itembutton">
+                      <div class="btn">
+                        <span> <i class="ph-handbag"></i>ADD TO CART</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+    
+    `
+    document
+      .querySelector('.productbestseller .products__list-boxitem')
+      .insertAdjacentHTML('beforeend', template)
+
+    val.size.forEach(function (el) {
+      let template2 = `<li><a href="">${el}</a></li>`
+      document
+        .querySelectorAll('.products__list-size ul')
+        [val.id].insertAdjacentHTML('beforeend', template2)
+    })
+  })
+}
+// document.querySelector('.products__list-size ul').firstChild.className('active')
+renderProductsBestSeller()
